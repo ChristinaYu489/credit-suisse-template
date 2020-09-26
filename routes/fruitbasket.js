c94bca99074ca303e36e07cd36c9d7d2f1fd9a87
ChristinaYu489 <983165011@qq.com>
2020-09-26 17:33:03 +0800

    add log

--- routes/fruitbasket.js
+++ routes/fruitbasket.js
@@ -4,7 +4,7 @@ var router = Router();
 router.post('/', function (req, res) {
     var basket = req.body;
 
-    // console.log("My result is--> %s", req.body);
+    console.log("My result is--> %s", basket);
     let body = 240;
     res.send('240');
 });
